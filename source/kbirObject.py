4c65fec8eb6e0af524007d6a45bc4d8b20e42e30
Reed Swiernik <rswiernik@csh.rit.edu>
2016-04-04 19:51:15 -0400

    I'm bad at changing small things

--- source/kbirObject.py
+++ source/kbirObject.py
@@ -2,12 +2,13 @@
 
 class KeyboardRepresentation:
 	def __init__(self, name, cols, rows):
-		self.name    = name
+		self.name       = name
 		self.numColumns = cols
 		self.numRows    = rows
-		self.keyMatrix = [][]
+		self.keyMatrix  = [][]
+		self.layers     = []
 
-	def addKey(key, row):
+	def addKey(key, layer, row):
 		keyMatrix[row].append(key)
 
 	def __str__(self):
