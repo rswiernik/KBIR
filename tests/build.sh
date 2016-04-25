df7cc3e21a1faff7adcd8f56c35fa2e9f676be4e
Reed Swiernik <rswiernik@csh.rit.edu>
2016-04-25 16:01:20 -0400

    Add better build and file support

--- tests/build.sh
+++ tests/build.sh
@@ -5,6 +5,19 @@ if [[ $# -ne 0 ]]; then
 	exit 0
 fi
 
+OUTPUT_FILE="output.layout"
+
+if [[ -e ${OUTPUT_FILE} ]]; then
+	read -p "File ${OUTPUT_FILE} exists. Would you like to delete it? [Y/n]" -n 1 -r
+	echo
+	if [[ $REPLY =~ ^[Yy]$ ]]; then
+		echo "rm ${OUTPUT_FILE}"
+		rm "${OUTPUT_FILE}"
+	else
+		echo "Leaving file, build might fail."
+	fi
+fi
+
 echo "Alright, let's do this... LEEROY"
 
 python source/kbirparse.py -k layouts/planck_standard.kbl -v
